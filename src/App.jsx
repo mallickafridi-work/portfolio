e626df724b3e280b88fc4b0f9bfc291c44ff9c4d
Afridi Mallick <mallickafridi.work@gmail.com>
2026-03-03 01:21:25 +0530

    Added Initial Layout (Header, Main Content Area and Footer)

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ function App() {
 
   return (
     <>
-      <div className='main container'>
-        <h1 className='title'>Hello World</h1>
+      <div className='main'>
+        <header className='header'>
+          <nav className='navbar'>
+            NAVBAR
+          </nav>
+        </header>
+        <div className="navbar"></div>
+        <div className="content-area">
+          main content area
+        </div>
+        <footer className='footer'>FOOTER</footer>
       </div>
     </>
   )
