d07b8c226c7b9920b3916c459fe4e8939ec96eda
Afridi Mallick <mallickafridi.work@gmail.com>
2026-03-05 03:52:07 +0530

    Designed the Navbar for Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from 'react'
 import backVideo from './assets/video/space.mp4'
 
 function App() {
 
+  const [clicked, setClicked] = useState(false)
+  const [active, setActive] = useState('')
+
+  function handleClick() {
+    clicked ? setClicked(false) : setClicked(true)
+  }
+
   return (
     <>
+      <video autoPlay loop muted playsInline className="back-video" src={backVideo}></video>
       <div className='main'>
         <header className='header'>
-          <nav className='navbar'>
-            <a className='item px-6 text-white hidden ' href="/">About</a>
-            <a className='item px-6 text-white ' href="/">Contact</a>
-            <a className='item px-6 text-white ' href="/">Projects</a>
+
+          <nav className={`nav-item navbar ${clicked ? 'active' : ''}`}>
+            <a className='item px-6' href="/"> About </a>
+            <a className='item px-6' href="/"> Contact </a>
+            <a className='item px-6' href="/"> Projects </a>
           </nav>
-          <i class="fa-solid fa-bars"></i>
+          <div className="nav-item menu">
+            {
+              clicked ?
+                <i className="fa-solid fa-x" type='button' onClick={handleClick}></i> :
+                <i className="fa-solid fa-bars" type='button' onClick={handleClick}></i>
+            }
+
+
+          </div>
         </header>
-        <div className="navbar"></div>
+
         <div className="content-area">
           main content area
         </div>
-        <footer className='footer'>FOOTER</footer>
+
+        <footer className='footer'> FOOTER </footer>
+
       </div>
     </>
   )
